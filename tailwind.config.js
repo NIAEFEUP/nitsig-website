c8339250160a7c52e2de574ecb4437cf694e0c72
toino <joaoapereira21@hotmail.com>
2023-09-20 16:11:41 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["index.html"],
+    theme: {
+        extend: {
+            colors: {
+                red: "#f06666",
+                bgOne: "#ffffff",
+                bgTwo: "#f7f9f9",
+                accentOne: "#536471",
+                accentTwo: "#dbe4e6",
+                accentThree: "#993333",
+                accentFour: "#e04a4a",
+                accentFive: "#e04a4a",
+
+                bgOneDark: "#231012",
+                bgTwoDark: "#402629",
+                accentOneDark: "#e9dde0",
+                accentTwoDark: "#c78f8f",
+            },
+        },
+    },
+    plugins: [],
+};
